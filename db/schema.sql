57976f8114134d8f739f4a2582e6f28def3f7339
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-04-29 11:15:21 -0700

    JOINed tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,23 @@
-CREATE TABLE candidates (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    industry_connected BOOLEAN NOT NULL 
-);
+-- drop tables
+-- candidates table must be dropped first because of foreign key constraint
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
 
+-- create parties table
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT
 );
+
+-- creates table candidates
+-- candidates table must come after parties table because of foreign key constraint
+CREATE TABLE candidates (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    party_id INTEGER,
+    industry_connected BOOLEAN NOT NULL,
+    CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
